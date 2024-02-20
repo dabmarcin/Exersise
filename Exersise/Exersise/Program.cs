b9415ff93e48959fd86798631cb87357c15e1d4b
Marcin Dabrowski <dabmarcin@gmail.com>
2024-02-20 19:01:17 +0100

    task5

--- Exersise/Exersise/Program.cs
+++ Exersise/Exersise/Program.cs
@@ -1,20 +1,10 @@
 ﻿using System;
 
-int[] grades = new int[365];
-List<string> dayOfWeeks = new List<string>();
-dayOfWeeks.Add("poniedzialek");
-dayOfWeeks.Add("wtorek");
-dayOfWeeks.Add("sroda");
-dayOfWeeks.Add("czwartek");
-dayOfWeeks.Add("piatek");
-dayOfWeeks.Add("sobota");
-dayOfWeeks.Add("niedziela");
-//Console.WriteLine(dayOfWeeks[2]);
-
-//string[] dayOfWeeks2 = { "poniedzialek", "wtorek", "sroda", "czwartek", "piatek", "sobota", "niedziela" };
-//Console.WriteLine(dayOfWeeks[0]);
-
-foreach (var day in dayOfWeeks)
+public class Exersise
 {
-    Console.WriteLine(day);
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Podaj liczbe");
+        int x = Convert.ToInt32(Console.ReadLine());
+    }
 }
